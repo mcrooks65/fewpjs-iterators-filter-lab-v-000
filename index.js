63f359e6ddf0f9f1efaa81429e2f59d86a9e6f8e
Matthew Crooks <mfcrooks@gmail.com>
2019-10-06 22:32:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-let even = arr.filter(n => {
-  return n % 2 === 0;
-});
-// even = [2,4,6]
-
 function findMatching (list, name) {
   return list.filter((driverName) => {
     return driverName.toLowerCase() === name.toLowerCase
