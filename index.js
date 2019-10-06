c42b8e8c0a5b5429a77f9835d7bb17f324eea99e
Matthew Crooks <mfcrooks@gmail.com>
2019-10-06 22:33:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function findMatching (list, name) {
   return list.filter(function (driverName) {
-    return driverName.toLowerCase() === name.toLowerCase
+    return driverName.toLowerCase() === name.toLowerCase();
   });
 }
