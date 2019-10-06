cdc622c1a926242e9e7b6e9c2426ac988dcf5aea
Matthew Crooks <mfcrooks@gmail.com>
2019-10-06 22:29:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solution here
+let even = arr.filter(n => {
+  return n % 2 === 0;
+});
+// even = [2,4,6]
+
+let findMatching = drivers.filter(string => {
+  return string == drivers[string]
+})
